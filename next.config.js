444d695d9a61106b1b555d858b2f5aea9972729a
Jenn C <jenn8866@gmail.com>
2021-12-27 13:09:38 -0600

    Test out external image domain loading

--- next.config.js
+++ next.config.js
@@ -38,4 +38,7 @@ module.exports = withBundleAnalyzer({
 
     return config
   },
+  images: {
+    domains: ['blue-bottle-cms.global.ssl.fastly.net', 'en.timemore.com', 'i5.walmartimages.com'],
+  },
 })
